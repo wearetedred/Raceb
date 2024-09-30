3c0c8cba7213a35b028a4e17f7507339597820c0
Ubaid Ullah | Web Developer & Coder <126957292+theubcoder@users.noreply.github.com>
2024-09-30 14:29:07 +0500

    Added a welcome message to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to tedred</h1>
+      <h2>Good Day</h2>
       <Image src="./images/my-image.jpg" alt="My Image" width={500} height={500} />
     </div>
   );
