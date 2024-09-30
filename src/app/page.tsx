2c3613ffebbdc6153609ae9729feb912e2723f3c
Ubaid Ullah | Web Developer & Coder <126957292+theubcoder@users.noreply.github.com>
2024-09-30 14:08:02 +0500

    files and folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1 className="text-2xl font-bold">My next js Application </h1>
-      </main>
-      
+    <div>
+      <h1>Welcome to tedred</h1>
+      <Image src="./images/my-image.jpg" alt="My Image" width={500} height={500} />
     </div>
   );
 }
