acd136357ccb79b5b1c8a0fcd48a38e31711204e
Hamdullah <hamdullahs046@gmail.com>
2024-09-30 15:48:30 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to tedred</h1>
+      <h1>Welcome to tedred hamd</h1>
       <Image src="./images/my-image.jpg" alt="My Image" width={500} height={500} />
     </div>
   );
